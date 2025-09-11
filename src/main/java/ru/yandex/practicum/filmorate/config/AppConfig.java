11221d364a086275f2982fb64caecfc81a05dcc6
LogosTaurbek <taurbek@yandex.ru>
2025-09-12 01:15:19 +0600

    add filmorate

---
+++ src/main/java/ru/yandex/practicum/filmorate/config/AppConfig.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Setter
+@Getter
+@ToString
+@ConfigurationProperties("filmorate")
+public class AppConfig {
+    private int defaultNumberOfTopFilms;
+
+    public int getDefaultNumberOfTopFilms () {
+        return defaultNumberOfTopFilms;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Film> getAllFilms() {
+        return this.filmService.getAllFilms();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Film getFilmById(@PathVariable int id) {
+        return this.filmService.getFilmById(id);
+    }
+
+    @GetMapping("/popular")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Film> getTopLikedFilms(@RequestParam(required = false) int count) {
+        return this.filmService.getTopLikedFilms(count);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film addFilm(@RequestBody Film newFilm) {
+        return this.filmService.addFilm(newFilm);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Film updateFilm(@RequestBody Film updatedFilm) {
+        return this.filmService.updateFilm(updatedFilm);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void addLike(@PathVariable int userId, @PathVariable int id) {
+        this.filmService.addLike(userId, id);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeLike(@PathVariable int userId, @PathVariable int id) {
+        this.filmService.removeLike(userId, id);
+    }
 }
