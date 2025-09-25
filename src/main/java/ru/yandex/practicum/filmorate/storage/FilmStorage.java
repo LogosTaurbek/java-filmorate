6ddd4c0b78379262ae9a7fced48d0846b2096a4a
m.taurbek <m.taurbek@alem.kz>
2025-09-25 14:25:08 +0500

    reformat code

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +9,7 @@ public interface FilmStorage {
     Film addFilm(Film newFilm);
 
     Film updateFilm(Film updatedFilm);
-    //Optional<List<User>> getAllUsers();
+
     Optional<List<Film>> getAllFilms();
 
     Film getFilmById(int id);
