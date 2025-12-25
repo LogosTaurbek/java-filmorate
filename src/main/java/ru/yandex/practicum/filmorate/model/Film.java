6fb3ef6f2b2dde301894e4e085bfca0bc49bef1e
m.taurbek <m.taurbek@alem.kz>
2025-12-25 13:50:00 +0500

    clear unused imports

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,12 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import ru.yandex.practicum.filmorate.exceptions.UserValidationException;
 
 import java.time.LocalDate;
-import java.util.HashSet;
+
 import java.util.List;
 import java.util.Set;
 
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.time.LocalDate;
